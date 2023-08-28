c3c23676d8b4960179123ca711e8c87a40d38b29
Ryan F <ryan.friers@torontofilmschool.ca>
2023-08-28 14:41:22 -0400

    Added Pause Menu
    - Created pause menu with settings
    - Linked pause menu to main menu
    - Pause menu is in scene - (RyanF)
    - To use pause menu in other scenes, just copy the canvas and event manager, paste in scene of choice

---
+++ Assets/UI/Scripts/StatHolder.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatHolder
+{
+    public static float brightness = 0.5f;
+    public static float volume = 0.5f;
+}
